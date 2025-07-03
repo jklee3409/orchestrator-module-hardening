e489f3ac9a866aa548b14bf9aee8595ecce367d2
𝑠𝑒𝑜𝑏𝑜𝑖𝑛 <sbi1024@naver.com>
2025-07-03 20:53:13 +0900

    feat: 테스트

--- src/main/java/eureca/capstone/project/orchestrator/controller/TestController.java
+++ src/main/java/eureca/capstone/project/orchestrator/controller/TestController.java
@@ -19,6 +19,7 @@ public class TestController {
 
     @GetMapping("/healthCheck")
     public String healthCheck() {
+
         log.trace("healthCheck");
         log.debug("healthCheck");
         log.info("healthCheck");
