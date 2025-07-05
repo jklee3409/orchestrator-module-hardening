fb117387d1a0a634074074de11355d3b9ddb8124
𝑠𝑒𝑜𝑏𝑜𝑖𝑛 <sbi1024@naver.com>
2025-07-05 14:43:27 +0900

    feat : start-server.sh 파일 수정

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -19,7 +19,7 @@ if ! command -v aws &> /dev/null; then
 fi
 
 aws ecr get-login-password --region ap-northeast-2 | \
-docker login --username AWS --password-stdin 221082211696.dkr.ecr.ap-northeast-2.amazonaws.com
+sudo docker login --username AWS --password-stdin 221082211696.dkr.ecr.ap-northeast-2.amazonaws.com
 sudo docker stop capstone-server || true
 sudo docker rm capstone-server || true
 sudo docker pull 221082211696.dkr.ecr.ap-northeast-2.amazonaws.com/capstone-server:latest
