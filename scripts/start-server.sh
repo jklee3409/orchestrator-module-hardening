37bcb367a7dccfb7421e0d080585d9b0fdfa8f26
𝑠𝑒𝑜𝑏𝑜𝑖𝑛 <sbi1024@naver.com>
2025-07-05 11:58:45 +0900

    feat : start-server.sh 파일 수정

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "--------------- 서버 배포 시작 -----------------"
-docker stop capstone-server || true
-docker rm capstone-server || true
-docker pull 221082211696.dkr.ecr.ap-northeast-2.amazonaws.com/capstone-server:latest
-docker run -d --name capstone-server -p 8080:8080 221082211696.dkr.ecr.ap-northeast-2.amazonaws.com/capstone-server:latest
+sudo docker stop capstone-server || true
+sudo docker rm capstone-server || true
+sudo docker pull 221082211696.dkr.ecr.ap-northeast-2.amazonaws.com/capstone-server:latest
+sudo docker run -d --name capstone-server -p 8080:8080 221082211696.dkr.ecr.ap-northeast-2.amazonaws.com/capstone-server:latest
 echo "--------------- 서버 배포 끝 -----------------"
