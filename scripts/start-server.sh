56b7632f89ccd46b736e153fc3ecfc658e8a4d30
𝑠𝑒𝑜𝑏𝑜𝑖𝑛 <sbi1024@naver.com>
2025-07-05 13:18:50 +0900

    feat : start-server.sh 파일 수정

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 echo "--------------- 서버 배포 시작 -----------------"
+sudo apt update
+sudo apt install -y awscli
 aws ecr get-login-password --region ap-northeast-2 | \
 docker login --username AWS --password-stdin 221082211696.dkr.ecr.ap-northeast-2.amazonaws.com
 sudo docker stop capstone-server || true
