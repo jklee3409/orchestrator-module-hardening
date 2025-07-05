fe1bae51f3d1a3c9ec23fd7605979f47b36ef5c6
𝑠𝑒𝑜𝑏𝑜𝑖𝑛 <sbi1024@naver.com>
2025-07-05 13:32:09 +0900

    feat : start-server.sh 파일 수정

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 echo "--------------- 서버 배포 시작 -----------------"
-sudo apt update
-sudo apt install -y awscli
+# AWS CLI 설치 (ubuntu 24.04에서는 apt로 설치 불가)
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip -q awscliv2.zip
+sudo ./aws/install
 aws ecr get-login-password --region ap-northeast-2 | \
 docker login --username AWS --password-stdin 221082211696.dkr.ecr.ap-northeast-2.amazonaws.com
 sudo docker stop capstone-server || true
