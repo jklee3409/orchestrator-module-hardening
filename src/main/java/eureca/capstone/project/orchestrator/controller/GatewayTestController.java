79349219bec776dfbb976231bb3cbb0aff062d14
𝑠𝑒𝑜𝑏𝑜𝑖𝑛 <sbi1024@naver.com>
2025-07-11 11:15:44 +0900

    feat : 게이트웨이 테스트를 위한 컨트롤러 어노테이션 추가

--- src/main/java/eureca/capstone/project/orchestrator/controller/GatewayTestController.java
+++ src/main/java/eureca/capstone/project/orchestrator/controller/GatewayTestController.java
@@ -1,8 +1,10 @@
 package eureca.capstone.project.orchestrator.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @RequestMapping("/orchestrator")
 public class GatewayTestController {
     @GetMapping("/test")
