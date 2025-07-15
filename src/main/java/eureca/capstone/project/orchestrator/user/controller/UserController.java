57f9339a30acbd8e7d64b5ad840d8facb197ce2a
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-07-15 22:01:09 +0900

    feat: 사용자 닉네임, 비밀번호 변경 기능 Controller Layer 구현

--- src/main/java/eureca/capstone/project/orchestrator/user/controller/UserController.java
+++ src/main/java/eureca/capstone/project/orchestrator/user/controller/UserController.java
@@ -3,14 +3,19 @@ package eureca.capstone.project.orchestrator.user.controller;
 import eureca.capstone.project.orchestrator.common.dto.base.BaseResponseDto;
 import eureca.capstone.project.orchestrator.user.dto.request.user.CreateUserRequestDto;
 import eureca.capstone.project.orchestrator.user.dto.request.user.GetUserProfileRequestDto;
+import eureca.capstone.project.orchestrator.user.dto.request.user.UpdateNicknameRequestDto;
+import eureca.capstone.project.orchestrator.user.dto.request.user.UpdatePasswordRequestDto;
 import eureca.capstone.project.orchestrator.user.dto.response.user.CreateUserResponseDto;
 import eureca.capstone.project.orchestrator.user.dto.response.user.GetUserProfileResponseDto;
+import eureca.capstone.project.orchestrator.user.dto.response.user.UpdateNicknameResponseDto;
+import eureca.capstone.project.orchestrator.user.dto.response.user.UpdatePasswordResponseDto;
 import eureca.capstone.project.orchestrator.user.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,5 +39,19 @@ public class UserController {
     public BaseResponseDto<GetUserProfileResponseDto> getUserProfile(@Valid GetUserProfileRequestDto getUserProfileRequestDto) {
         GetUserProfileResponseDto getUserProfileResponseDto = userService.getUserProfile(getUserProfileRequestDto);
         return BaseResponseDto.success(getUserProfileResponseDto);
+    }
+
+    @PutMapping("/nickname")
+    @Operation(summary = "사용자 닉네임 변경", description = "userId 에 해당하는 사용자의 닉네임을 변경합니다.")
+    public BaseResponseDto<UpdateNicknameResponseDto> updateUserNickname(@Valid @RequestBody UpdateNicknameRequestDto updateUserNicknameRequestDto) {
+        UpdateNicknameResponseDto updateUserNicknameResponseDto = userService.updateUserNickname(updateUserNicknameRequestDto);
+        return BaseResponseDto.success(updateUserNicknameResponseDto);
+    }
+
+    @PutMapping("/password")
+    @Operation(summary = "사용자 비밀번호 변경", description = "userId 또는 이메일에 해당하는 사용자의 비밀번호를 변경합니다.")
+    public BaseResponseDto<UpdatePasswordResponseDto> updateUserPassword(@Valid @RequestBody UpdatePasswordRequestDto request) {
+        UpdatePasswordResponseDto response = userService.updateUserPassword(request);
+        return BaseResponseDto.success(response);
     }
 }
