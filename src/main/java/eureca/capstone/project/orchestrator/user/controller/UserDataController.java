9bcc01854e72f9ac9e0fa422d227c5f83554707d
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-07-15 22:08:36 +0900

    feat: 보유 데이터 -> 판매 가능 데이터 전환 기능 Controller Layer 구현

--- src/main/java/eureca/capstone/project/orchestrator/user/controller/UserDataController.java
+++ src/main/java/eureca/capstone/project/orchestrator/user/controller/UserDataController.java
@@ -1,13 +1,17 @@
 package eureca.capstone.project.orchestrator.user.controller;
 
 import eureca.capstone.project.orchestrator.common.dto.base.BaseResponseDto;
+import eureca.capstone.project.orchestrator.user.dto.request.user_data.CreateSellableDataRequestDto;
 import eureca.capstone.project.orchestrator.user.dto.request.user_data.GetUserDataStatusRequestDto;
+import eureca.capstone.project.orchestrator.user.dto.response.user_data.CreateSellableDataResponseDto;
 import eureca.capstone.project.orchestrator.user.dto.response.user_data.GetUserDataStatusResponseDto;
 import eureca.capstone.project.orchestrator.user.service.UserDataService;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +26,12 @@ public class UserDataController {
     public BaseResponseDto<GetUserDataStatusResponseDto> getUserDataStatus(@Valid GetUserDataStatusRequestDto requestDto) {
         GetUserDataStatusResponseDto responseDto = userDataService.getUserDataStatus(requestDto);
         return BaseResponseDto.success(responseDto);
+    }
+
+    @PutMapping("/enable-sale/change")
+    @Operation(summary = "사용자 보유 데이터 -> 판매 가능 데이터로 전환", description = "사용자의 보유 데이터를 판매 가능 데이터로 전환합니다.")
+    public BaseResponseDto<CreateSellableDataResponseDto> createSellableData(@RequestBody CreateSellableDataRequestDto createSellableDataRequestDto) {
+        CreateSellableDataResponseDto createSellableDataResponseDto = userDataService.createSellableData(createSellableDataRequestDto);
+        return BaseResponseDto.success(createSellableDataResponseDto);
     }
 }
