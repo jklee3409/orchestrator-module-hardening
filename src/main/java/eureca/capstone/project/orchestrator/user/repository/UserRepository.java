cd2af17492d2ec5d915a1bb55635564f328490d3
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-07-15 22:20:12 +0900

    feat: 사용자 수 조회 기능 Repository Layer 구현

--- src/main/java/eureca/capstone/project/orchestrator/user/repository/UserRepository.java
+++ src/main/java/eureca/capstone/project/orchestrator/user/repository/UserRepository.java
@@ -1,10 +1,16 @@
 package eureca.capstone.project.orchestrator.user.repository;
 
+import eureca.capstone.project.orchestrator.common.entity.Status;
 import eureca.capstone.project.orchestrator.user.entity.User;
 import eureca.capstone.project.orchestrator.user.repository.custom.UserRepositoryCustom;
+import java.time.LocalDateTime;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long>, UserRepositoryCustom {
     Optional<User> findByEmail(String email);
+
+    long countByStatus(Status status);
+
+    long countByCreatedAtBetween(LocalDateTime start, LocalDateTime end);
 }
