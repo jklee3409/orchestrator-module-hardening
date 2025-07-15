0575fc6c2945fd4500fb286ec7eeda8369e48e22
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-07-15 16:57:14 +0900

    feat: 사용자 회원가입 기능 Dto Layer 구현

---
+++ src/main/java/eureca/capstone/project/orchestrator/user/dto/response/user/CreateUserResponseDto.java
@@ -0,0 +1,10 @@
+package eureca.capstone.project.orchestrator.user.dto.response.user;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class CreateUserResponseDto {
+    private Long id;
+}
