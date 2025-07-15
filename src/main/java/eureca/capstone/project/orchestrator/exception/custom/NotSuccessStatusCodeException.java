57de3417f865a469386d4762118185924617e179
𝑠𝑒𝑜𝑏𝑜𝑖𝑛 <sbi1024@naver.com>
2025-07-15 09:25:40 +0900

    fix : 프로젝트 구조 초기화

--- src/main/java/eureca/capstone/project/orchestrator/exception/custom/NotSuccessStatusCodeException.java
+++
@@ -1,7 +0,0 @@
-package eureca.capstone.project.orchestrator.exception.custom;
-
-public class NotSuccessStatusCodeException extends RuntimeException {
-    public NotSuccessStatusCodeException() {
-        super("API 결과가 정상적이지 않습니다.");
-    }
-}
