60c62dc0d99fc4ff397e8a3b9aec70ef22f90b3e
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-07-16 23:32:41 +0900

    [VT2-70] feat: 게시글 상세 조회 기능 Dto Layer 구현

--- src/main/java/eureca/capstone/project/orchestrator/user/dto/response/user/GetUserProfileResponseDto.java
+++ src/main/java/eureca/capstone/project/orchestrator/user/dto/response/user/GetUserProfileResponseDto.java
@@ -1,5 +1,6 @@
 package eureca.capstone.project.orchestrator.user.dto.response.user;
 
+import eureca.capstone.project.orchestrator.common.dto.TelecomCompanyDto;
 import eureca.capstone.project.orchestrator.user.entity.User;
 import lombok.Builder;
 import lombok.Data;
@@ -12,24 +13,12 @@ public class GetUserProfileResponseDto {
     private String phoneNumber;
     private TelecomCompanyDto telecomCompany;
 
-    @Data
-    @Builder
-    private static class TelecomCompanyDto {
-        private Long telecomCompanyId;
-        private String name;
-    }
-
     public static GetUserProfileResponseDto fromUser(User user) {
-        TelecomCompanyDto telecom = TelecomCompanyDto.builder()
-                .telecomCompanyId(user.getTelecomCompany().getTelecomCompanyId())
-                .name(user.getTelecomCompany().getName())
-                .build();
-
         return GetUserProfileResponseDto.builder()
                 .nickname(user.getNickname())
                 .email(user.getEmail())
                 .phoneNumber(user.getPhoneNumber())
-                .telecomCompany(telecom)
+                .telecomCompany(TelecomCompanyDto.fromEntity(user.getTelecomCompany()))
                 .build();
     }
 }
