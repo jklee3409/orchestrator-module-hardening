6f0d9fd2165213860166600c3b3d9f9f0eb853c5
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-07-16 20:23:38 +0900

    [VT2-72] feat: 판매글 수정 기능 Repository Layer 구현

---
+++ src/main/java/eureca/capstone/project/orchestrator/transaction_feed/repository/custom/TransactionFeedRepositoryCustom.java
@@ -0,0 +1,8 @@
+package eureca.capstone.project.orchestrator.transaction_feed.repository.custom;
+
+import eureca.capstone.project.orchestrator.transaction_feed.entity.TransactionFeed;
+import java.util.Optional;
+
+public interface TransactionFeedRepositoryCustom {
+    Optional<TransactionFeed> findByIdWithLock(Long transactionFeedId);
+}
