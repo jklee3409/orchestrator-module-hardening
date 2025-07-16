e32e333dcea3590c9dee68b7a89a3b050fa7fac0
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-07-16 23:33:01 +0900

    [VT2-70] feat: 게시글 상세 조회 기능 Repository Layer 구현

--- src/main/java/eureca/capstone/project/orchestrator/transaction_feed/repository/impl/TransactionFeedRepositoryImpl.java
+++ src/main/java/eureca/capstone/project/orchestrator/transaction_feed/repository/impl/TransactionFeedRepositoryImpl.java
@@ -1,6 +1,10 @@
 package eureca.capstone.project.orchestrator.transaction_feed.repository.impl;
 
+import static eureca.capstone.project.orchestrator.common.entity.QStatus.status;
+import static eureca.capstone.project.orchestrator.common.entity.QTelecomCompany.telecomCompany;
+import static eureca.capstone.project.orchestrator.transaction_feed.entity.QSalesType.salesType;
 import static eureca.capstone.project.orchestrator.transaction_feed.entity.QTransactionFeed.transactionFeed;
+import static eureca.capstone.project.orchestrator.user.entity.QUser.user;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import eureca.capstone.project.orchestrator.transaction_feed.entity.TransactionFeed;
@@ -22,6 +26,21 @@ public class TransactionFeedRepositoryImpl implements TransactionFeedRepositoryC
         TransactionFeed result = jpaQueryFactory
                 .selectFrom(transactionFeed)
                 .where(transactionFeed.transactionFeedId.eq(transactionFeedId))
+                .fetchOne();
+
+        return Optional.ofNullable(result);
+    }
+
+    @Override
+    public Optional<TransactionFeed> findFeedDetailById(Long transactionFeedId) {
+        TransactionFeed result = jpaQueryFactory
+                .selectFrom(transactionFeed)
+                .leftJoin(transactionFeed.user, user).fetchJoin()
+                .leftJoin(transactionFeed.salesType, salesType).fetchJoin()
+                .leftJoin(transactionFeed.telecomCompany, telecomCompany).fetchJoin()
+                .leftJoin(transactionFeed.status, status).fetchJoin()
+                .where(transactionFeed.transactionFeedId.eq(transactionFeedId)
+                        .and(transactionFeed.isDeleted.eq(false)))
                 .fetchOne();
 
         return Optional.ofNullable(result);
