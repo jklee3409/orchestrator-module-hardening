d98f9472807a4817779b7d753ca361ffbcfe1198
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-07-16 20:22:24 +0900

    [VT2-72] feat: 커스텀 예외 전역 처리되도록 추가

--- src/main/java/eureca/capstone/project/orchestrator/common/exception/GlobalExceptionHandler.java
+++ src/main/java/eureca/capstone/project/orchestrator/common/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package eureca.capstone.project.orchestrator.common.exception;
 import eureca.capstone.project.orchestrator.common.dto.base.BaseResponseDto;
 import eureca.capstone.project.orchestrator.common.dto.base.ErrorResponseDto;
 import eureca.capstone.project.orchestrator.common.exception.code.ErrorCode;
+import eureca.capstone.project.orchestrator.common.exception.custom.*;
 import eureca.capstone.project.orchestrator.common.exception.custom.BlackListUserException;
 import eureca.capstone.project.orchestrator.common.exception.custom.EmailVerifyTokenMismatchException;
 import eureca.capstone.project.orchestrator.common.exception.custom.RefreshTokenMismatchException;
@@ -31,5 +32,71 @@ public class GlobalExceptionHandler {
     public BaseResponseDto<ErrorResponseDto> handleBlockOAuthUserException(BlackListUserException e) {
         log.error(e.getMessage(), e);
         return BaseResponseDto.fail(ErrorCode.BLACK_LIST_USER_FOUND);
+    }
+
+    @ExceptionHandler(FeedModifyPermissionException.class)
+    public BaseResponseDto<ErrorResponseDto> handleFeedModifyPermissionException(FeedModifyPermissionException e) {
+        log.error(e.getMessage(), e);
+        return BaseResponseDto.fail(ErrorCode.FEED_MODIFY_PERMISSION_DENIED);
+    }
+
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    public BaseResponseDto<ErrorResponseDto> handleEmailAlreadyExistsException(EmailAlreadyExistsException e) {
+        log.error(e.getMessage(), e);
+        return BaseResponseDto.fail(ErrorCode.EMAIL_ALREADY_EXISTS);
+    }
+
+    @ExceptionHandler(InternalServerException.class)
+    public BaseResponseDto<ErrorResponseDto> handleInternalServerException(InternalServerException e) {
+        log.error(e.getMessage(), e);
+        return BaseResponseDto.fail(e.getErrorCode());
+    }
+
+    @ExceptionHandler(DataOverSellableAmountException.class)
+    public BaseResponseDto<ErrorResponseDto> handleDataOverSellableAmountException(DataOverSellableAmountException e) {
+        log.error(e.getMessage(), e);
+        return BaseResponseDto.fail(ErrorCode.USER_SELLABLE_DATA_LACK);
+    }
+
+    @ExceptionHandler(EmptyPlanException.class)
+    public BaseResponseDto<ErrorResponseDto> handleEmptyPlanException(EmptyPlanException e) {
+        log.error(e.getMessage(), e);
+        return BaseResponseDto.fail(ErrorCode.EMPTY_PLAN);
+    }
+
+    @ExceptionHandler(InvalidTelecomCompanyException.class)
+    public BaseResponseDto<ErrorResponseDto> handleInvalidTelecomCompanyException(InvalidTelecomCompanyException e) {
+        log.error(e.getMessage(), e);
+        return BaseResponseDto.fail(ErrorCode.INVALID_TELECOM_COMPANY);
+    }
+
+    @ExceptionHandler(TelecomCompanyNotFoundException.class)
+    public BaseResponseDto<ErrorResponseDto> handleTelecomCompanyNotFoundException(TelecomCompanyNotFoundException e) {
+        log.error(e.getMessage(), e);
+        return BaseResponseDto.fail(ErrorCode.TELECOM_COMPANY_NOT_FOUND);
+    }
+
+    @ExceptionHandler(StatusNotFoundException.class)
+    public BaseResponseDto<ErrorResponseDto> handleStatusNotFoundException(StatusNotFoundException e) {
+        log.error(e.getMessage(), e);
+        return BaseResponseDto.fail(ErrorCode.STATUS_NOT_FOUND);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public BaseResponseDto<ErrorResponseDto> handleUserNotFoundException(UserNotFoundException e) {
+        log.error(e.getMessage(), e);
+        return BaseResponseDto.fail(ErrorCode.USER_NOT_FOUND);
+    }
+
+    @ExceptionHandler(TransactionFeedNotFoundException.class)
+    public BaseResponseDto<ErrorResponseDto> handleTransactionFeedNotFoundException(TransactionFeedNotFoundException e) {
+        log.error(e.getMessage(), e);
+        return BaseResponseDto.fail(ErrorCode.TRANSACTION_FEED_NOT_FOUND);
+    }
+
+    @ExceptionHandler(UserDataNotFoundException.class)
+    public BaseResponseDto<ErrorResponseDto> handleUserDataNotFoundException(UserDataNotFoundException e) {
+        log.error(e.getMessage(), e);
+        return BaseResponseDto.fail(ErrorCode.USER_DATA_NOT_FOUND);
     }
 }
