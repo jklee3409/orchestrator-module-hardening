43e55c96ecf84f7bff6a77f42c92ea6accc697a2
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-07-17 21:51:05 +0900

    [VT2-77] feat: 결제 검증을 위한 주문 정보 생성 기능 Repository Layer 구현

---
+++ src/main/java/eureca/capstone/project/orchestrator/pay/repository/ChargeHistoryRepository.java
@@ -0,0 +1,7 @@
+package eureca.capstone.project.orchestrator.pay.repository;
+
+import eureca.capstone.project.orchestrator.pay.entity.ChargeHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChargeHistoryRepository extends JpaRepository<ChargeHistory, Long> {
+}
