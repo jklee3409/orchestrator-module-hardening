6a49007cae190e445d51e74f9e66373098cc604a
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-07-17 21:51:16 +0900

    [VT2-77] feat: 결제 검증을 위한 주문 정보 생성 기능 Service Layer 구현

--- src/main/java/eureca/capstone/project/orchestrator/pay/service/PaymentService.java
+++ src/main/java/eureca/capstone/project/orchestrator/pay/service/PaymentService.java
@@ -1,8 +1,11 @@
 package eureca.capstone.project.orchestrator.pay.service;
 
 import eureca.capstone.project.orchestrator.pay.dto.request.CouponCalculationRequestDto;
+import eureca.capstone.project.orchestrator.pay.dto.request.PaymentPrepareRequestDto;
 import eureca.capstone.project.orchestrator.pay.dto.response.CouponCalculationResponseDto;
+import eureca.capstone.project.orchestrator.pay.dto.response.PaymentPrepareResponseDto;
 
 public interface PaymentService {
     CouponCalculationResponseDto calculateDiscount(String email, CouponCalculationRequestDto couponCalculationRequestDto);
+    PaymentPrepareResponseDto preparePayment(String email, PaymentPrepareRequestDto paymentPrepareRequestDto );
 }
--- src/main/java/eureca/capstone/project/orchestrator/pay/service/impl/PaymentServiceImpl.java
+++ src/main/java/eureca/capstone/project/orchestrator/pay/service/impl/PaymentServiceImpl.java
@@ -1,11 +1,17 @@
 package eureca.capstone.project.orchestrator.pay.service.impl;
 
+import eureca.capstone.project.orchestrator.common.entity.Status;
+import eureca.capstone.project.orchestrator.common.exception.code.ErrorCode;
+import eureca.capstone.project.orchestrator.common.exception.custom.InternalServerException;
 import eureca.capstone.project.orchestrator.common.exception.custom.UserNotFoundException;
 import eureca.capstone.project.orchestrator.common.util.PayTypeManager;
 import eureca.capstone.project.orchestrator.common.util.StatusManager;
 import eureca.capstone.project.orchestrator.pay.dto.PayTypeDto;
 import eureca.capstone.project.orchestrator.pay.dto.request.CouponCalculationRequestDto;
+import eureca.capstone.project.orchestrator.pay.dto.request.PaymentPrepareRequestDto;
 import eureca.capstone.project.orchestrator.pay.dto.response.CouponCalculationResponseDto;
+import eureca.capstone.project.orchestrator.pay.dto.response.PaymentPrepareResponseDto;
+import eureca.capstone.project.orchestrator.pay.entity.ChargeHistory;
 import eureca.capstone.project.orchestrator.pay.entity.PayType;
 import eureca.capstone.project.orchestrator.pay.entity.UserEventCoupon;
 import eureca.capstone.project.orchestrator.pay.repository.ChargeHistoryRepository;
@@ -14,6 +20,8 @@ import eureca.capstone.project.orchestrator.pay.service.PaymentService;
 import eureca.capstone.project.orchestrator.pay.service.UserEventCouponService;
 import eureca.capstone.project.orchestrator.user.entity.User;
 import eureca.capstone.project.orchestrator.user.repository.UserRepository;
+import java.time.LocalDateTime;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -57,8 +65,61 @@ public class PaymentServiceImpl implements PaymentService {
                 .build();
     }
 
+    @Override
+    @Transactional
+    public PaymentPrepareResponseDto preparePayment(String email, PaymentPrepareRequestDto requestDto) {
+        log.info("[preparePayment] 결제 검증을 위한 주문 정보 생성 요청");
+
+        User user = findUserByEmail(email);
+        UserEventCoupon coupon = null;
+        Long discountAmount = 0L;
+        PayType requiredPayType = null;
+
+        if (requestDto.getUserEventCouponId() != null) {
+            log.info("[preparePayment] 이벤트 쿠폰 적용 요청");
+            coupon = userEventCouponService.validateAndGetCoupon(requestDto.getUserEventCouponId(), user);
+
+            double discountRate = coupon.getEventCoupon().getDiscountRate() / 100.0;
+            double calculateDiscount = requestDto.getOriginalAmount() * discountRate;
+
+            discountAmount = Math.round(calculateDiscount);
+            log.info("[preparePayment] 할인 금액 계산 검증. 할인 금액: {}", discountAmount);
+
+            requiredPayType = payTypeManager.getPayType(coupon.getEventCoupon().getPayType().getName());
+            log.info("[preparePayment] 쿠폰 사용을 위한 결제 수단: {}", requiredPayType.getName());
+        }
+
+        Long expectedFinalAmount = requestDto.getFinalAmount() - discountAmount;
+        if (!expectedFinalAmount.equals(requestDto.getFinalAmount())) throw new InternalServerException(ErrorCode.FINAL_AMOUNT_NOT_MATCHED);
+        log.info("[preparePayment] 요청 결제 금액과 일치");
+
+        ChargeHistory chargeHistory = createRequestChargeHistory(requestDto.getOriginalAmount(), discountAmount, requestDto.getFinalAmount(), user, coupon);
+        chargeHistoryRepository.save(chargeHistory);
+        log.info("[preparePayment] 결제 전 주문 정보 생성 및 저장 완료");
+
+        return new PaymentPrepareResponseDto(chargeHistory, requiredPayType);
+    }
+
     private User findUserByEmail(String email) {
         return userRepository.findByEmail(email)
                 .orElseThrow(UserNotFoundException::new);
+    }
+
+    private ChargeHistory createRequestChargeHistory(Long originalAmount, Long discountAmount, Long finalAmount, User user, UserEventCoupon coupon) {
+        log.info("[createRequestChargeHistory] 충전 내역 생성 시작 - 결제 전 주문 정보만 생성");
+
+        Status requestedStatus = statusManager.getStatus("PAYMENT", "REQUESTED");
+        String orderId = UUID.randomUUID().toString();
+
+        return ChargeHistory.builder()
+                .user(user)
+                .userEventCoupon(coupon)
+                .orderId(orderId)
+                .amount(originalAmount)
+                .chargePay(finalAmount)
+                .discountAmount(discountAmount)
+                .status(requestedStatus)
+                .requestedAt(LocalDateTime.now())
+                .build();
     }
 }
