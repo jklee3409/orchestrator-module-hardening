392c518131dcae1f3ae778b3432bbf88c4f684f1
𝑠𝑒𝑜𝑏𝑜𝑖𝑛 <sbi1024@naver.com>
2025-07-18 12:01:08 +0900

    feat : whitelist 검증 로직 변경

--- src/main/java/eureca/capstone/project/orchestrator/auth/constant/FilterConstant.java
+++ src/main/java/eureca/capstone/project/orchestrator/auth/constant/FilterConstant.java
@@ -12,8 +12,6 @@ public class FilterConstant {
 
             "/login/oauth2/code/kakao",
             "/oauth2/authorization/kakao",
-            "/orchestrator/login/oauth2/code/kakao",
-            "/orchestrator/oauth2/authorization/kakao",
 
             "/error",
             "/favicon.ico",
