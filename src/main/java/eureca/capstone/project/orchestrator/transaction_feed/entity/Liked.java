340fb90b8216d82c6731cd7218e6d7ea7e1d97cf
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-07-19 20:59:19 +0900

    [VT2-67] feat: 판매글 찜 등록 및 삭제 기능 Dto Layer 구현

--- src/main/java/eureca/capstone/project/orchestrator/transaction_feed/entity/Liked.java
+++ src/main/java/eureca/capstone/project/orchestrator/transaction_feed/entity/Liked.java
@@ -3,9 +3,18 @@ package eureca.capstone.project.orchestrator.transaction_feed.entity;
 import eureca.capstone.project.orchestrator.common.entity.BaseEntity;
 import eureca.capstone.project.orchestrator.user.entity.User;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Table(name = "liked")
 @Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Table(name = "liked")
 public class Liked extends BaseEntity {
     @Column(name = "liked_id")
     @Id
