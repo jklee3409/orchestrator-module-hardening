b920f6f8d0989e737f05c02d7a8abe4d6fa83aca
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-07-19 17:14:26 +0900

    [VT2-52] feat: ElasticSearch Document 추가

---
+++ src/main/java/eureca/capstone/project/orchestrator/transaction_feed/document/TransactionFeedDocument.java
@@ -0,0 +1,86 @@
+package eureca.capstone.project.orchestrator.transaction_feed.document;
+
+import eureca.capstone.project.orchestrator.transaction_feed.entity.TransactionFeed;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.data.elasticsearch.annotations.Setting;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(indexName = "transaction_feed")
+@Setting(settingPath = "elasticsearch/analyzer-settings.json")
+public class TransactionFeedDocument {
+
+    @Id
+    private Long id;
+
+    @Field(type = FieldType.Text, analyzer = "nori")
+    private String title;
+
+    @Field(type = FieldType.Text, analyzer = "nori")
+    private String content;
+
+    @Field(type = FieldType.Long)
+    private Long salesPrice;
+
+    @Field(type = FieldType.Long)
+    private Long salesDataAmount;
+
+    @Field(type = FieldType.Keyword)
+    private Long sellerId;
+
+    @Field(type = FieldType.Keyword)
+    private String nickname;
+
+    @Field(type = FieldType.Keyword)
+    private Long telecomCompanyId;
+
+    @Field(type = FieldType.Text, analyzer = "nori")
+    private String telecomCompanyName; // 검색용
+
+    @Field(type = FieldType.Keyword)
+    private Long salesTypeId;
+
+    @Field(type = FieldType.Keyword)
+    private String status;
+
+    @Field(type = FieldType.Long)
+    private Long defaultImageNumber;
+
+    @Field(type = FieldType.Date, format = {}, pattern = "uuuu-MM-dd'T'HH:mm:ss.SSS")
+    private LocalDateTime createdAt;
+
+    @Field(type = FieldType.Date, format = {}, pattern = "uuuu-MM-dd'T'HH:mm:ss.SSS")
+    private LocalDateTime expiresAt;
+
+    @Field(type = FieldType.Boolean)
+    private boolean isDeleted;
+    
+    public static TransactionFeedDocument fromEntity(TransactionFeed transactionFeed) {
+        return TransactionFeedDocument.builder()
+                .id(transactionFeed.getTransactionFeedId())
+                .title(transactionFeed.getTitle())
+                .content(transactionFeed.getContent())
+                .salesPrice(transactionFeed.getSalesPrice())
+                .salesDataAmount(transactionFeed.getSalesDataAmount())
+                .sellerId(transactionFeed.getUser().getUserId())
+                .nickname(transactionFeed.getUser().getNickname())
+                .telecomCompanyId(transactionFeed.getTelecomCompany().getTelecomCompanyId())
+                .telecomCompanyName(transactionFeed.getTelecomCompany().getName())
+                .salesTypeId(transactionFeed.getSalesType().getSalesTypeId())
+                .status(transactionFeed.getStatus().getCode())
+                .createdAt(transactionFeed.getCreatedAt())
+                .expiresAt(transactionFeed.getExpiresAt())
+                .isDeleted(transactionFeed.isDeleted())
+                .build();
+    }
+}
