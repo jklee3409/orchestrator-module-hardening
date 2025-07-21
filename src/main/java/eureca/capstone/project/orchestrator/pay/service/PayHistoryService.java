429f4c326a2c872323535a201384170af39dffc0
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-07-21 22:25:31 +0900

    [VT2-78] feat: 페이 변동 내역 추가 기능 구현

--- src/main/java/eureca/capstone/project/orchestrator/pay/service/PayHistoryService.java
+++ src/main/java/eureca/capstone/project/orchestrator/pay/service/PayHistoryService.java
@@ -1,8 +1,11 @@
 package eureca.capstone.project.orchestrator.pay.service;
 
 import eureca.capstone.project.orchestrator.pay.entity.ChargeHistory;
+import eureca.capstone.project.orchestrator.transaction_feed.entity.DataTransactionHistory;
 import eureca.capstone.project.orchestrator.user.entity.User;
 
 public interface PayHistoryService {
     void createChargePayHistory(User user, Long changedPay, Long finalPay, ChargeHistory chargeHistory);
+    void createPurchasePayHistory(User buyer, Long changedPay, DataTransactionHistory dataTransactionHistory);
+    void createSalePayHistory(User seller, Long changedPay, DataTransactionHistory transactionHistory);
 }
