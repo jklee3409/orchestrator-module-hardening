fa22d71d43e10eb2c7f7276a051e432fa1882af3
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-07-22 10:06:21 +0900

    [VT2-251] feat: 사용자 데이터 쿠폰 목록 조회 기능 Repository Layer 구현

--- src/main/java/eureca/capstone/project/orchestrator/transaction_feed/repository/UserDataCouponRepository.java
+++ src/main/java/eureca/capstone/project/orchestrator/transaction_feed/repository/UserDataCouponRepository.java
@@ -1,7 +1,8 @@
 package eureca.capstone.project.orchestrator.transaction_feed.repository;
 
 import eureca.capstone.project.orchestrator.transaction_feed.entity.UserDataCoupon;
+import eureca.capstone.project.orchestrator.transaction_feed.repository.custom.UserDataCouponRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserDataCouponRepository extends JpaRepository<UserDataCoupon, Long> {
+public interface UserDataCouponRepository extends JpaRepository<UserDataCoupon, Long>, UserDataCouponRepositoryCustom {
 }
