a2d8699dd8b372db156aac578395a3082bc9263d
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-07-23 18:24:52 +0900

    [VT2-280] feat: 거래 내역 조회 API Service Layer 구현

--- src/main/java/eureca/capstone/project/orchestrator/transaction_feed/service/DataTransactionHistoryService.java
+++ src/main/java/eureca/capstone/project/orchestrator/transaction_feed/service/DataTransactionHistoryService.java
@@ -1,10 +1,15 @@
 package eureca.capstone.project.orchestrator.transaction_feed.service;
 
+import eureca.capstone.project.orchestrator.transaction_feed.dto.enums.TransactionHistoryType;
+import eureca.capstone.project.orchestrator.transaction_feed.dto.response.GetTransactionHistoryResponseDto;
 import eureca.capstone.project.orchestrator.transaction_feed.entity.DataTransactionHistory;
 import eureca.capstone.project.orchestrator.transaction_feed.entity.TransactionFeed;
 import eureca.capstone.project.orchestrator.user.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface DataTransactionHistoryService {
     DataTransactionHistory createNormalTransactionHistory(User buyer, TransactionFeed transactionFeed);
     DataTransactionHistory createAuctionTransactionHistory(User buyer, TransactionFeed transactionFeed, Long finalPrice);
+    Page<GetTransactionHistoryResponseDto> getTransactionHistory(String email, TransactionHistoryType type, Pageable pageable);
 }
