3484eace67ec564cd5969aafbb8da166f4138304
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-07-23 22:13:56 +0900

    [VT2-281] feat: 페이 변동 내역 조회 API Entity Layer

--- src/main/java/eureca/capstone/project/orchestrator/pay/entity/Bank.java
+++ src/main/java/eureca/capstone/project/orchestrator/pay/entity/Bank.java
@@ -6,8 +6,17 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @Table(name = "bank")
 public class Bank extends BaseEntity {
 
