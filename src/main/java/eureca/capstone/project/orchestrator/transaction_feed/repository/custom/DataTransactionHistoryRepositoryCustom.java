30acb9b2efc8e3c8007f6976bd7a7e57eaffa743
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-07-23 18:24:44 +0900

    [VT2-280] feat: 거래 내역 조회 API Repository Layer 구현

--- src/main/java/eureca/capstone/project/orchestrator/transaction_feed/repository/custom/DataTransactionHistoryRepositoryCustom.java
+++ src/main/java/eureca/capstone/project/orchestrator/transaction_feed/repository/custom/DataTransactionHistoryRepositoryCustom.java
@@ -1,9 +1,15 @@
 package eureca.capstone.project.orchestrator.transaction_feed.repository.custom;
 
 import eureca.capstone.project.orchestrator.transaction_feed.dto.UserTransactionAverageDto;
+import eureca.capstone.project.orchestrator.transaction_feed.dto.enums.TransactionHistoryType;
+import eureca.capstone.project.orchestrator.transaction_feed.dto.response.GetTransactionHistoryResponseDto;
+import eureca.capstone.project.orchestrator.transaction_feed.entity.DataTransactionHistory;
 import eureca.capstone.project.orchestrator.user.entity.User;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface DataTransactionHistoryRepositoryCustom {
     Optional<UserTransactionAverageDto> findAverageByUser(User user);
+    Page<DataTransactionHistory> findTransactionHistoryByUserId(Long userId, TransactionHistoryType type, Pageable pageable);
 }
