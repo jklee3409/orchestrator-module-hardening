31b9aaaa93101b9213da059120de0def6907be26
Jang-GO <jang8195@naver.com>
2025-07-25 18:06:07 +0900

    feat: 비밀번호 재설정용 const추가

--- src/main/java/eureca/capstone/project/orchestrator/common/constant/UrlConstant.java
+++ src/main/java/eureca/capstone/project/orchestrator/common/constant/UrlConstant.java
@@ -3,4 +3,5 @@ package eureca.capstone.project.orchestrator.common.constant;
 public class UrlConstant {
     public static final String PRODUCT_FRONT_URL = "https://ureca-final.com/";
     public static final String VERIFY_EMAIL_TOKEN_URL = "https://www.visiblego.com/orchestrator/auth/verify-email?token=";
+    public static final String RESET_PASSWORD_TOKEN_URL = "https://www.visiblego.com/orchestrator/user/password-reset/form?token=";
 }
