6fa5771af11089d96329f6cd6f7542e24a4f7061
𝑠𝑒𝑜𝑏𝑜𝑖𝑛 <sbi1024@naver.com>
2025-08-01 17:02:05 +0900

    feat : API 컨트롤러에 권한 체크 어노테이션 부여

--- src/main/java/eureca/capstone/project/orchestrator/pay/controller/UserPayController.java
+++ src/main/java/eureca/capstone/project/orchestrator/pay/controller/UserPayController.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
