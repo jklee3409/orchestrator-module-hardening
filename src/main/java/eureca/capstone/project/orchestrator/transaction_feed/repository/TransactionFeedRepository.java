d2671180d2dc60c3d5aace6d8dc9241b598db5b3
Jong Kyu Lee <jklee3409@khu.ac.kr>
2026-04-02 17:12:25 +0900

    fix(repository): TransactionFeedRepository @Param import 누락 수정

--- src/main/java/eureca/capstone/project/orchestrator/transaction_feed/repository/TransactionFeedRepository.java
+++ src/main/java/eureca/capstone/project/orchestrator/transaction_feed/repository/TransactionFeedRepository.java
@@ -2,12 +2,12 @@ package eureca.capstone.project.orchestrator.transaction_feed.repository;
 
 import eureca.capstone.project.orchestrator.transaction_feed.entity.TransactionFeed;
 import eureca.capstone.project.orchestrator.transaction_feed.repository.custom.TransactionFeedRepositoryCustom;
-import io.lettuce.core.dynamic.annotation.Param;
 import jakarta.persistence.LockModeType;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface TransactionFeedRepository extends JpaRepository<TransactionFeed, Long>, TransactionFeedRepositoryCustom {
     @Lock(LockModeType.PESSIMISTIC_WRITE)
