1c7469373b1a4d079a921abd75e720226581fed4
Jong Kyu Lee <jklee3409@khu.ac.kr>
2026-04-02 16:23:57 +0900

    feat(repository): 입찰 및 거래 피드 처리를 위한 BidsRepository, TransactionFeedRepository 메서드 추가

--- src/main/java/eureca/capstone/project/orchestrator/transaction_feed/repository/TransactionFeedRepository.java
+++ src/main/java/eureca/capstone/project/orchestrator/transaction_feed/repository/TransactionFeedRepository.java
@@ -2,7 +2,19 @@ package eureca.capstone.project.orchestrator.transaction_feed.repository;
 
 import eureca.capstone.project.orchestrator.transaction_feed.entity.TransactionFeed;
 import eureca.capstone.project.orchestrator.transaction_feed.repository.custom.TransactionFeedRepositoryCustom;
+import io.lettuce.core.dynamic.annotation.Param;
+import jakarta.persistence.LockModeType;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TransactionFeedRepository extends JpaRepository<TransactionFeed, Long>, TransactionFeedRepositoryCustom {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("""
+            select tf
+              from TransactionFeed tf
+             where tf.transactionFeedId = :transactionFeedId
+            """)
+    Optional<TransactionFeed> findByIdForUpdate(@Param("transactionFeedId") Long transactionFeedId);
 }
